819a11289b15ec7392f44611a2af74952c11c95c
Jack <jack@solidpower.email>
2024-08-22 19:10:39 +0200

    add reviews

--- components/Slides/Reviews.vue
+++ components/Slides/Reviews.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container m-auto flex items-center full-height" data-swiper-parallax-y="-10%" data-swiper-parallax-opacity="0" data-swiper-parallax-duration="400">
     <div class="content">
-      <Decorative :content="'p'" :className="'tag-item__text'" :tag="'wrap'">
+      <Decorative :content="'h2'" :className="'tag-item__text'" :tag="'wrap'">
+        <h2>Reviews</h2>
+      </Decorative>
+      <Decorative :content="'main'" :className="'tag-item__text'" :tag="'wrap'">
         Reviews
       </Decorative>
     </div>
--- data/reviews.ts
+++ data/reviews.ts
@@ -1,11 +1,119 @@
 export const data = [
   {
-    name: 'Mark Smith',
+    name: {
+      en: 'Dmytro Tetiura',
+      ua: 'Дмитро Тетюра',
+      pl: 'Dmytro Tetiura'
+    },
     avatar: '',
     review: {
-      en: 'Server rendered, progressive web application for online fitness & health coaching. Providing workout programs, nutritions plans, workout logs, follow up with client\'s progress and more.',
-      ua: 'Серверний рендеринг, прогресивний веб-застосунок для онлайн тренувань і оздоровчого коучингу. Пропонує тренувальні програми, плани харчування, журнали тренувань, відстеження прогресу клієнтів та багато іншого.',
-      pl: 'Aplikacja internetowa z renderowaniem po stronie serwera, progresywna aplikacja internetowa do treningów i coachingu zdrowotnego online. Oferuje programy treningowe, plany żywieniowe, dzienniki treningowe, śledzenie postępów klientów i wiele więcej.'
+      en: '<p>A responsive and pleasant young man who easily and quickly created the project. He is always available and ready to do his work with quality. I bothered him for a long time with various corrections and changes that needed to be made, and he did everything perfectly, fixed and adjusted everything according to my wishes without any issues. He knows his job excellently, always ready to suggest and advise how to do better. I recommend Yevhen for cooperation, and I will definitely reach out to him again.</p>',
+      ua: '<p>Відповідальний і приємний молодий чоловік, який легко і швидко створив проєкт, завжди знаходиться на зв’язку та готовий виконати свою роботу якісно. Тривалий час «діставав» його різними правками та змінами, які необхідно було внести, і він усе зробив якісно, виправив та налаштував за моїми побажаннями без жодних проблем. Чудово знає свою справу, завжди готовий підказати та порадити, як зробити краще. Рекомендую Євгена до співпраці, обов’язково звертатимусь до нього ще.</p>',
+      pl: '<p>Uczynny i miły młody człowiek, który szybko i sprawnie stworzył projekt. Zawsze dostępny i gotowy do wykonania swojej pracy na wysokim poziomie. Przez długi czas "nękałem" go różnymi poprawkami i zmianami, które trzeba było wprowadzić, a on wszystko zrobił starannie, poprawił i dostosował zgodnie z moimi życzeniami bez żadnych problemów. Zna swoją pracę doskonale, zawsze gotowy doradzić i zasugerować, jak zrobić lepiej. Polecam Eugeniusza do współpracy, na pewno jeszcze do niego wrócę.</p>'
     }
+  },
+  {
+    name: {
+      en: 'Nina Podolian',
+      ua: 'Ніна Подолян',
+      pl: 'Nina Podolian'
+    },
+    avatar: 'images/reviews/ninapodolyan.webp',
+    review: {
+      en: '<p>I needed to fix the layout on a custom-built website. Yevhen provided clear and expert consultation and took on the job.</p><p>Since I am very far from programming, layout design, and so on, I explained what I wanted as best as I could, and he understood me. He also did a lot of extra work that wasn\'t included in the initial specifications, yet he kept the price the same.</p><p>I think the job wasn\'t easy, as the website was built a long time ago on a custom CMS. Yevhen patiently handled the non-flexible layout and worked with me. Always in touch. I recommend him!</p>',
+      ua: '<p>Потрібно було виправити верстку на самописному сайті. Євген чітко і експертно проконсультував мене та взявся за роботу.</p><p>Оскільки я дуже далека від програмування, верстки тощо, я пояснювала, що хочу, як могла, і він мене розумів. Він зробив багато додаткових моментів, які не входили в технічне завдання, а ціну залишив ту саму.</p><p>Робота, думаю, була не з простих, оскільки сайт написаний давно і на самописній CMS. Євген терпляче впорався з нерезиновою версткою і зі мною. Завжди на зв’язку. Рекомендую!</p>',
+      pl: '<p>Trzeba było poprawić układ na autorsko napisanej stronie. Eugeniusz jasno i profesjonalnie mnie doradził i podjął się pracy.</p><p>Ponieważ jestem bardzo daleka od programowania, układów i tym podobnych rzeczy, wyjaśniałam, co chcę, jak mogłam, a on mnie rozumiał. Zrobił też wiele dodatkowych rzeczy, które nie były w specyfikacji, a cenę pozostawił taką samą.</p><p>Myślę, że praca nie była łatwa, ponieważ strona została napisana dawno temu na autorskim CMS. Eugeniusz cierpliwie poradził sobie z nierozciągliwym układem i ze mną. Zawsze w kontakcie. Polecam!</p>'
+    }
+  },
+  {
+    name: {
+      en: 'Vitalii Mazur',
+      ua: 'Віталій Мазур',
+      pl: 'Witalij Mazur'
+    },
+    avatar: '',
+    review: {
+      en: '<p>Everything went as well as possible. All wishes were taken into account, even those I forgot to include in the project requirements.</p>',
+      ua: '<p>Все пройшло максимально добре. Були враховані всі побажання, навіть ті, про які забув згадати в умовах проєкту.</p>',
+      pl: '<p>Wszystko poszło maksymalnie dobrze. Uwzględniono wszystkie życzenia, nawet te, o których zapomniałem wspomnieć w warunkach projektu.</p>'
+    }
+  },
+  {
+    name: {
+      en: 'Oleg Pavenskii',
+      ua: 'Олег Павенський',
+      pl: 'Oleg Pawenśki'
+    },
+    avatar: 'images/reviews/olegpavenskii.webp',
+    review: {
+      en: '<p>Good work completed within reasonable deadlines. Yevhen delivered the tasks with high quality and met all deadlines. Communication was smooth, and all feedback was addressed. I recommend him for his professional approach and reliability.</p>',
+      ua: '<p>Хороша робота в прийнятні строки. Євгеній якісно виконав завдання і дотримався всіх термінів. Комунікація була зручною, і всі зауваження були враховані. Рекомендую за професійний підхід і відповідальність.</p>',
+      pl: '<p>Dobra praca wykonana w rozsądnych terminach. Eugeniusz dostarczył zadania wysokiej jakości i dotrzymał wszystkich terminów. Komunikacja była płynna, a wszystkie uwagi zostały uwzględnione. Polecam za profesjonalne podejście i odpowiedzialność.</p>'
+    }
+  },
+  {
+    name: {
+      en: 'Alexander Samoilov',
+      ua: 'Олександр Самойлов',
+      pl: 'Aleksander Samojłow'
+    },
+    avatar: '',
+    review: {
+      en: '<p>A MEGA-responsible person. He approached the website issue with understanding and achieved 10/10 for the visual and 10/10 for the functionality. He did more than we planned, so I\'m especially grateful!</p><p>He created a beautiful website header based on a Photoshop template!</p>',
+      ua: '<p>Людина МЕГА-відповідальна. З розумінням віднеслася до проблеми сайту і змогла досягти 10/10 по візуалу і 10/10 по функціоналу. Зробив більше, ніж планували, тому окремо дякую!</p><p>Зробив красиву шапку сайту за шаблоном з Photoshop!</p>',
+      pl: '<p>Osoba MEGA-odpowiedzialna. Z pełnym zrozumieniem podszedł do problemu strony i osiągnął 10/10 za wygląd i 10/10 za funkcjonalność. Zrobił więcej, niż planowaliśmy, więc serdecznie dziękuję!</p><p>Stworzył piękny nagłówek strony według szablonu z Photoshopa!</p>'
+    }
+  },
+  {
+    name: {
+      en: 'Alexander Markin',
+      ua: 'Олександр Маркін',
+      pl: 'Aleksander Markin'
+    },
+    avatar: '',
+    review: {
+      en: '<p>Yevhen is a very responsible performer who completed the task with high quality and on time.</p><p>He did more than required by the technical specifications, as I had some additional small tasks arise during the work. Yevhen handled these tasks with the same quality as the main ones.</p><p>I recommend him for collaboration. I will reach out again if needed.</p>',
+      ua: '<p>Євгеній — дуже відповідальний виконавець, який виконав завдання дуже якісно і в строк.</p><p>По технічному завданню зроблено більше, оскільки в процесі роботи виникли ще невеликі завдання, які потрібно було вирішити. Євгеній виконав їх не менш якісно, ніж основні.</p><p>Рекомендую для співпраці. Я звернуся ще, якщо знадобиться.</p>',
+      pl: '<p>Eugeniusz jest bardzo odpowiedzialnym wykonawcą, który zrealizował zadanie bardzo dobrze i w terminie.</p><p>Zrobił więcej, niż było wymagane w specyfikacji, ponieważ w trakcie pracy pojawiły się jeszcze drobne zadania do wykonania. Eugeniusz zrealizował je z równie wysoką jakością jak główne zadania.</p><p>Polecam do współpracy. Skontaktuję się ponownie, jeśli zajdzie taka potrzeba.</p>'
+    }
+  },
+  {
+    name: {
+      en: 'Svetlana Kiyarova',
+      ua: 'Світлана Кіярова',
+      pl: 'Swietłana Kijarowa'
+    },
+    avatar: '',
+    review: {
+      en: '<p>He demonstrated all the necessary competencies in his work. Completed everything in a short time. During the work, he identified a number of errors in the project. I recommend him as an excellent specialist.</p>',
+      ua: '<p>Проявив усі необхідні компетенції в роботі. Реалізував усе за короткий термін. У процесі роботи виявив ряд помилок у проєкті. Рекомендую як відмінного спеціаліста.</p>',
+      pl: '<p>Wykazał się wszystkimi niezbędnymi kompetencjami w pracy. Zrealizował wszystko w krótkim czasie. W trakcie pracy odkrył szereg błędów w projekcie. Polecam jako doskonałego specjalistę.</p>'
+    },
+  },
+  {
+    name: {
+      en: 'Yevhenii Parkhomenko',
+      ua: 'Євгеній Пархоменко',
+      pl: 'Jewhenij Parchomenko'
+    },
+    avatar: '',
+    review: {
+      en: '<p>Thank you very much; you couldn\'t find a better specialist. He didn\'t just create a website, he made a masterpiece.</p>',
+      ua: '<p>Дякую велике, кращого спеціаліста не знайти. Зробив не сайт, а цукерку.</p>',
+      pl: '<p>Dziękuję bardzo, lepszego specjalisty nie znajdziesz. Zrobił nie stronę, a cukierek.</p>'
+    },
+  },
+  {
+    name: {
+      en: 'Oleg Kudrenko',
+      ua: 'Олег Кудренко',
+      pl: 'Oleg Kudrenko'
+    },
+    avatar: '',
+    review: {
+      en: '<p>The project is completed, all comments were quickly and clearly addressed. Google Pagespeed - everything is in order. I recommend.</p>',
+      ua: '<p>Проєкт виконано, всі зауваження швидко і чітко доопрацьовані. Google Pagespeed - все в порядку. Рекомендую.</p>',
+      pl: '<p>Projekt wykonany, wszystkie uwagi szybko i dokładnie poprawione. Google Pagespeed - wszystko w porządku. Polecam.</p>'
+    },
   }
 ]
