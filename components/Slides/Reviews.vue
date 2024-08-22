72d42217f54042961eff425e04904c460fc04d62
Jack <jack@solidpower.email>
2024-08-23 00:46:38 +0200

    done reviews slider

--- components/Slides/Reviews.vue
+++ components/Slides/Reviews.vue
@@ -1,24 +1,67 @@
 <template>
   <div class="container m-auto flex items-center full-height" data-swiper-parallax-y="-10%" data-swiper-parallax-opacity="0" data-swiper-parallax-duration="400">
-    <div class="content container">
-      <Decorative :content="'h2'" :className="'tag-item__text'" :tag="'wrap'">
-        <h2>Reviews</h2>
-      </Decorative>
+    <div class="container">
       <Decorative :content="'main'" :className="'tag-item__text'" :tag="'wrap'">
+        <h2>Reviews</h2>
         <swiper
-          :pagination="pagination"
-          :slides-per-view="1"
-          :modules="modules"
           :direction="'vertical'"
+          :slidesPerView="'auto'"
           :freeMode="true"
-          :autoHeight="true"
-          :keyboard="true"
-          :spaceBetween="20"
+          :mousewheel="true"
+          :parallax="true"
+          :modules="modules"
+          :navigation="{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          }"
+          :pagination="pagination"
+          @reachEnd="handleFullSlider(true)"
+          @reachBeginning="handleFullSlider(true)"
+          @mouseenter="handleFullSlider(false)"
+          @mouseleave="handleFullSlider(true)"
+          class="mySwiper"
         >
         <swiper-slide v-for="(item, index) in data" :key="index">
-          <div v-html="item.review[$i18n.locale]"></div>
+          <div
+            class="review-inner flex rounded"
+            :style="{
+              margin: index % 2 == 0 ? '0 auto 0 0' : '0 0 0 auto'
+              }"
+            :data-swiper-parallax-x="index % 2 == 0 ? '-10%' : '10%'"
+            data-swiper-parallax-opacity="0"
+            data-swiper-parallax-duration="400"
+            >
+            <div class="review-avatar">
+              <img v-if="item.avatar" :src="item.avatar" alt="photo">
+              <IconUser v-if="!item.avatar"/>
+            </div>
+            <div class="review-body">
+              <h5 v-html="item.name[$i18n.locale]"/>
+              <div class="review-content" v-html="item.review[$i18n.locale]"/>
+            </div>
+            <svg v-if="index % 2 == 0" style="left: 2%;" width="49" height="22" viewBox="0 0 49 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M8.53944 2.41819C0.912333 7.4308 0.0219806 17.1314 0.000404575 21.1682C-0.0023175 21.6775 0.703969 21.8504 0.984679 21.4254C3.88326 17.0373 14.1245 4.99099 39.2039 4.991C69.8684 4.99101 18.3259 -4.0136 8.53944 2.41819Z" fill="var(--review-color)"/>
+            </svg>
+            <svg v-if="index % 2 != 0" style="right: 2%;" width="49" height="22" viewBox="0 0 49 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M40.4605 2.41819C48.0876 7.4308 48.978 17.1314 48.9996 21.1682C49.0023 21.6775 48.296 21.8504 48.0153 21.4254C45.1167 17.0373 34.8755 4.99099 9.79603 4.991C-20.8685 4.99101 30.674 -4.0136 40.4605 2.41819Z" fill="var(--review-color)"/>
+            </svg>
+          </div>
         </swiper-slide>
+        <swiper-slide style="height: 150px;"></swiper-slide>
         </swiper>
+        <div class="swiper-navigation flex items-center">
+          <div class="swiper-button-prev flex">
+            <svg width="40" height="21" viewBox="0 0 40 21" fill="none" xmlns="http://www.w3.org/2000/svg" style="transform: scale(-1,1);">
+              <path d="M1 10.5H39M39 10.5L29.5 1M39 10.5L29.5 20" stroke="var(--secondary-color)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
+          <div class="swiper-button-next flex">
+            <svg width="40" height="21" viewBox="0 0 40 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 10.5H39M39 10.5L29.5 1M39 10.5L29.5 20" stroke="var(--secondary-color)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
+        </div>
+        <div class="reviews-pagination"></div>
       </Decorative>
     </div>
   </div>
@@ -27,24 +70,37 @@
 <script>
 
   import Decorative from '~/components/DecorativeWrapper.vue';
+  import IconUser from '~/components/Svg/User.vue';
   import { Swiper, SwiperSlide } from 'swiper/vue';
-  import { Keyboard, FreeMode, Pagination, Navigation, Grid } from 'swiper/modules';
+  import { FreeMode, Mousewheel, Navigation, Pagination, Parallax } from 'swiper/modules';
   import { data } from '~/data/reviews'
+
+  import { handleWheel, enableSwiper } from '~/utils/globalFunctions.js';
 
   export default {
     data() {
       return {
         data,
-        modules: [Keyboard, FreeMode, Pagination, Navigation, Grid],
+        modules: [FreeMode, Mousewheel, Navigation, Pagination, Parallax],
+        pagination: {
+          el: ".reviews-pagination",
+          clickable: true,
+        },
       }
     },
     components: {
       Decorative,
       Swiper,
-      SwiperSlide
+      SwiperSlide,
+      IconUser
     },
     mounted() {
-      console.log(this.data)
+      // console.log(this.data)
+    },
+    methods: {
+      handleFullSlider: function(val) {
+        this.$store.commit('setAllowMouseScroll', val);
+      }
     }
   };
 
@@ -53,7 +109,7 @@
 <style lang="scss" scoped>
   h2 {
     font-size: vw_big_screen(32px);
-    margin: 0;
+    margin: vw_big_screen(14px);
     font-weight: 900;
   }
   .container {
@@ -62,16 +118,45 @@
 
   .swiper {
     width: 100%;
-    // height: 400px;
     margin-left: 0;
-    // overflow: unset;
+    max-height: vw_big_screen(280px);
     padding-bottom: vw_big_screen(14px);
+    mask-image: linear-gradient(180deg,transparent 0,#000 10%,#000 90%,transparent);
     &-slide {
-      // height: calc((100% - 60px) / 3) !important;
-      height: fit-content !important;
-      width: 100%;
-      background: red;
+      width: calc(100% - vw_big_screen(10px));
+      margin-top: vw_big_screen(24px);
+    }
+  }
 
+  .review {
+    &-inner {
+      background-color: var(--review-color);
+      filter: drop-shadow(vw_big_screen(8px) vw_big_screen(14px) vw_big_screen(4px) var(--shadow-slide));
+      width: 80%;
+      position: relative;
+      padding: vw_big_screen(22px);
+      & svg {
+        position: absolute;
+        width: vw_big_screen(49px);
+        bottom: 0;
+        transform: translateY(78%);
+      }
+    }
+    &-avatar {
+      width: vw_big_screen(60px);
+      height: vw_big_screen(60px);
+      border-radius: 50%;
+      overflow: hidden;
+      margin-right: vw_big_screen(24px);
+      border: vw_big_screen(2px) solid var(--sidebar-color);
+    }
+    &-body {
+      width: 90%;
+      & h5 {
+        margin: 0 0 vw_big_screen(12px);
+        font-size: vw_big_screen(16px);
+        font-weight: 800;
+      }
     }
   }
 
--- components/Svg/User.vue
+++ components/Svg/User.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex items-center justify-center">
-    <svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="flex" width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g id="bgCarrier" stroke-width="0"></g>
       <g id="tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.528"></g>
       <g id="iconCarrier">
-        <path d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-        <path d="M20.5899 22C20.5899 18.13 16.7399 15 11.9999 15C7.25991 15 3.40991 18.13 3.40991 22" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z" stroke="var(--sidebar-color)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M20.5899 22C20.5899 18.13 16.7399 15 11.9999 15C7.25991 15 3.40991 18.13 3.40991 22" stroke="var(--sidebar-color)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
       </g>
     </svg>
   </div>
@@ -13,11 +13,10 @@
 
 <style lang="scss" scoped>
   div {
-    background-color: var(--button-color);
-    width: vw_big_screen(32px);
+    width: 100%;
     height: 100%;
   }
   svg {
-    width: vw_big_screen(20px);
+    width: 60%;
   }
 </style>
